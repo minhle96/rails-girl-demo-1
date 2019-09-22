45e6f49a1bfd36fc62c6a9068ce81c1e12cc661f
dathao <dathao262@gmail.com>
2019-09-22 16:32:30 +0700

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,31 @@
-product = Product.find(3)
-product.update!(description: "- An immersive Cinematic Infinity Display, Pro-grade Camera and Wireless PowerShare The next generation is here
-
-- Ultrasonic in-display fingerprint ID protects and unlocks with the first touch
-
-- Pro-grade Camera effortlessly captures epic, pro-quality images of the world as you see it
-
-- Intelligently accesses power by learning how and when you use your phone
-")
+Product.destroy_all
+Product.create(
+  name: 'Điện thoại iPhone XS Max 512GB',
+  brand: 'Apple',
+  price: 29000000,
+  image: 'https://cdn.fptshop.com.vn/Uploads/Originals/2018/10/11/636748771945393060_iPhone-Xs-Max-gold.png'
+)
+Product.create!(
+  name: 'Tai nghe Bluetooth Airpod 2',
+  brand: 'Apple',
+  price: 4500000,
+  image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/M/RX/MRXJ2/MRXJ2?wid=1144&hei=1144&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1551489675083'
+)
+Product.create!(
+  name: 'Điện Thoại Samsung Galaxy S10',
+  brand: 'Samsung',
+  price: 16000000,
+  image: 'https://cdn.fptshop.com.vn/Uploads/Originals/2019/2/21/636863643187455627_ss-galaxy-s10-trang-1.png'
+)
+Product.create!(
+  name: 'Tai nghe Beat Studio M8',
+  brand: 'Apple',
+  price: 7000000,
+  image: 'https://www.slickwraps.com/media/catalog/product/cache/1/image/580x580/9df78eab33525d08d6e5fb8d27136e95/b/e/beats-studio-3_camo-2_1.jpg'
+)
+Product.create!(
+  name: 'Máy tính bảng iPad Pro 11 inch Wifi 64GB',
+  brand: 'Apple',
+  price: 20000000,
+  image: 'https://cdn.tgdd.vn/Products/Images/522/195067/ipad-pro-11-inch-2018-64gb-wifi-33397-thumb-600x600.jpg'
+)
